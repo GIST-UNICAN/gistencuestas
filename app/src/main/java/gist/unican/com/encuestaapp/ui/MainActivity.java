793df1dc6d2c1e6e478f1fc9b18223f7ba5caaf2
Andres <rodriguezan@unican.es>
2017-05-11 12:15:53 +0200

    MAin view

--- app/src/main/java/gist/unican/com/encuestaapp/ui/MainActivity.java
+++ app/src/main/java/gist/unican/com/encuestaapp/ui/MainActivity.java
@@ -3,39 +3,23 @@ package gist.unican.com.encuestaapp.ui;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.siimkinks.sqlitemagic.SqliteMagic;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
+import butterknife.BindView;
 import gist.unican.com.encuestaapp.R;
-import gist.unican.com.encuestaapp.domain.model.SurveyObjectSend;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView()
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+       /*
         SurveyObjectSend sob=new SurveyObjectSend();
         sob.setAcoBe(1);
         String variable= "setAcoBe";
@@ -49,32 +33,9 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
-        }
+        }*/
         Context context = getApplicationContext();
         SqliteMagic.init((Application) context);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
